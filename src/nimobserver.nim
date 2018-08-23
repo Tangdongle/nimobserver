77a779539a002c5b7e9871e3f79df4668ad8af85
Ryanc_signiq <ryanc@signiq.com>
2018-08-23 11:21:46 +0800

    Moved files around into proper package

--- src/nimobserver.nim
+++ src/nimobserver.nim
@@ -1,3 +1,61 @@
 # messagebus
 # Copyright Ryanc_signiq
 # A message event queue implementation
+from threadpool import spawn
+from os import sleep
+from typetraits import name
+
+type
+  BaseObserver* = ref object of RootObj
+
+  Subject*[T] = object of RootObj
+    observers: seq[BaseObserver]
+    channel: Channel[T]
+
+  SubjectPtr*[T] = ptr Subject[T]
+
+method onNotify[T](observer: BaseObserver, message: T) {.base gcsafe.} = 
+  echo "Base Notification:: Data value: " & $message
+
+method notify[T](subject: SubjectPtr[T], notification: T) {.base gcsafe.} =
+  for observer in subject.observers:
+    observer.onNotify(notification[])
+
+method update[T](subject: SubjectPtr[T]) {.base thread.} =
+  while true:
+    let msg = recv(subject.channel)
+    subject.notify(msg)
+    echo "Sleeping"
+    sleep(300)
+
+method publish*[T](subject: SubjectPtr[T], message: T) {.base.} =
+  #[
+  Send our message through our channel, which will notify any observers
+  ]#
+  subject.channel.send message
+
+method addObserver*[T](subject: SubjectPtr[T], observer: BaseObserver) {.base.} =
+  subject.observers.add(observer)
+
+method removeObserver*[T](subject: SubjectPtr[T], observer: BaseObserver) {.base.} =
+  assert subject.observers.contains(observer)
+  subject.observers.delete(subject.observers.find(observer))
+
+proc newSubject*[T](): SubjectPtr[T] =
+  result = createShared(Subject[T])
+  result.observers = @[]
+  open(result.channel)
+
+proc initSubject*[T](): SubjectPtr[T] =
+  result = newSubject[T]()
+  spawn result.update()
+
+proc `$`*(x: BaseObserver): string =
+  name(type(x))
+
+proc `$`*[T](x: SubjectPtr[T]): string =
+  result = $x.observers
+
+#let globalTestSub: SubjectPtr[SlackMessagePtr] = newSubject[SlackMessagePtr]()
+#spawn globalTestSub.update()
+
--- tests/nimobserver_tests.nim
+++ tests/nimobserver_tests.nim
@@ -2,99 +2,35 @@ import unittest
 import asyncdispatch
 import os
 import sequtils, strutils
-import threadpool, locks
+import nimobserver
 import nimobserver/message
-
-type
-    BaseObserver = ref object of RootObj
-
-    TestObserver* = ref object of BaseObserver
-        inTesting: bool
-
-    Subject* = object of RootObj
-        observers: seq[BaseObserver]
-        channel: Channel[ptr SlackMessage]
-    SubjectPtr* = ptr Subject
-
-method onNotify(observer: BaseObserver, message: SlackMessage) {.base gcsafe.} = 
-    echo "Base Notification:: Data value: " & $message
-
-method onNotify(observer: TestObserver, message: SlackMessage) = 
-    #Test observer
-    if message.type == SlackRTMType.Test:
-        echo "Flipping testing!"
-        observer.inTesting = not observer.inTesting
-
-method notify(subject: SubjectPtr, notification: SlackMessage) {.base gcsafe.} =
-    var counter = 0
-    for observer in subject.observers:
-        inc counter
-        echo "Calling notify on observer #" & $counter
-        observer.onNotify(notification)
-
-method update(subject: SubjectPtr) {.base thread.} =
-    while true:
-        let msg = recv(subject.channel)
-        subject.notify(msg[])
-        echo "Sleeping"
-        sleep(300)
-
-#For non-blocking polling
-    #while true:
-    #    let (received, msg) = tryRecv(subject.channel)
-    #    echo received
-    #    if received:
-    #        subject.notify(msg[])
-    #    else:
-    #        echo "Sleeping"
-    #        sleep(300)
-
-method publish(subject: SubjectPtr, message: ptr SlackMessage) {.base.} =
-    #[
-    Add our events to the end of the queue, to be popped off the head when processed
-    ]#
-    subject.channel.send message
-
-proc newSubject(): SubjectPtr =
-    result = createShared(Subject)
-    result.observers = @[]
-    open(result.channel)
-
-method addObserver(subject: SubjectPtr, observer: BaseObserver) {.base.} =
-    subject.observers.add(observer)
-
-method removeObserver(subject: SubjectPtr, observer: BaseObserver) {.base.} =
-    assert subject.observers.contains(observer)
-    subject.observers.delete(subject.observers.find(observer))
-
-let globalTestSub: SubjectPtr = newSubject()
-
-proc `$`(x: SubjectPtr): string =
-    result = $x.observers.len
+from threadpool import spawn
 
 suite "MessageBusTests":
+  type
+    TestObserver = ref object of BaseObserver
+      inTesting: bool
 
-    setup:
-        echo "Setup"
+  setup:
+    echo "Setup"
+    let globalTestSub = initSubject[SlackMessagePtr]()
 
-    test "ChannelTests":
+  test "ChannelTests":
 
-        #Ask update() to run in another thead, where it will poll until it receives something
-        spawn globalTestSub.update()
-        var 
-            obs = TestObserver(inTesting: false)
-            obs2 = TestObserver(inTesting: false)
-            newMsg = newSlackMessage("test", "TestUser", "TestMessageText", "TestSendingUser")
+    var 
+      obs = TestObserver(inTesting: false)
+      obs2 = TestObserver(inTesting: false)
+      newMsg = newSlackMessage("test", "TestUser", "TestMessageText", "TestSendingUser")
 
-        proc updateGlobalSubject(sMsg: ptr SlackMessage) {.thread.} =
-            {.gcsafe.}:
-                globalTestSub.publish sMsg
+    proc updateGlobalSubject(sMsg: SlackMessagePtr) {.thread.} =
+      {.gcsafe.}:
+        globalTestSub.publish sMsg
 
-        globalTestSub.addObserver(obs)
-        globalTestSub.addObserver(obs2)
-        echo "Spawning two globalSubjects!"
-        spawn updateGlobalSubject(addr newMsg)
+    globalTestSub.addObserver(obs)
+    globalTestSub.addObserver(obs2)
+    echo "Spawning two globalSubjects!"
+    spawn updateGlobalSubject(addr newMsg)
 
-        echo "Sleeping in main"
-        sleep(3000)
-        spawn updateGlobalSubject(addr newMsg)
+    echo "Sleeping in main"
+    sleep(3000)
+    spawn updateGlobalSubject(addr newMsg)
